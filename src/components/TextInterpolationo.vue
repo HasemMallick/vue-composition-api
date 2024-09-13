2189064809b385ece3b94fa7ab0cbac085855ffc
Hasem Mallick <hasemmallick19@students.cutn.ac.in>
2024-09-13 07:19:17 +0530

    more text interpolation

--- src/components/TextInterpolationo.vue
+++ src/components/TextInterpolationo.vue
@@ -1,9 +1,18 @@
         <script setup>
             const msg = "this is a message"
+            const num = 252
+            const addNum = (x, y) => {
+                return x + y
+            }
         </script>
 <template>
     <div>
         <h2>This is interpolated text: <i>{{ msg }}</i> </h2>
+        <hr>
+        <p>Number : {{ num }}</p>
+        <hr>
+        <h3>Call a method</h3>
+        <h1>Add Number{{ addNum(2, 10) }}</h1>
     </div>
 </template>
 
