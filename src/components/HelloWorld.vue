ab6bf2fe73c7f28bc3de00c3172b7ad5e0aa6e00
Hasem Mallick <hasemmallick19@students.cutn.ac.in>
2024-09-13 06:35:33 +0530

    first commit

---
+++ src/components/HelloWorld.vue
@@ -0,0 +1,14 @@
+
+<template>
+    <div>
+        <h2>This is Hello Worl component</h2>
+    </div>
+</template>
+
+<script setup>
+console.log("Hello World")
+</script>
+
+<style lang="scss" scoped>
+
+</style>
