4298b8ddec1de7d8f8a51c5bd4b4bed3410d9800
Hasem Mallick <hasemmallick19@students.cutn.ac.in>
2024-09-13 10:37:26 +0530

    added more example of conditional redndering

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,12 @@
 <script setup>
 import ConditionalRendering from './components/ConditionalRendering.vue';
+import ConditionalRendering2 from './components/ConditionalRendering2.vue';
 
 
 
 </script>
 
 <template>
-  <ConditionalRendering />
+  <!-- <ConditionalRendering /> -->
+   <ConditionalRendering2 />
 </template>
---
+++ src/components/ConditionalRendering2.vue
@@ -0,0 +1,17 @@
+
+<script setup>
+import { ref} from 'vue';
+
+const paw = ref("sssword")
+</script>
+
+<template>
+    <div>
+        <h1 v-if="paw.length > 7" >Strong Password</h1>
+        <h1 v-else-if="paw.length > 1 && paw.length < 8">Password is Weak</h1>
+        <h1 v-else>Please provide a Password</h1>
+    </div>
+</template>
+<style lang="scss" scoped>
+
+</style>
