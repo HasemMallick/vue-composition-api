190258a7df72c7c8c945b58b35f1c0f54e05df14
Hasem Mallick <hasemmallick19@students.cutn.ac.in>
2024-09-13 08:28:30 +0530

    basic of event handler

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,8 @@
 <script setup>
-import CombinedStyle from './components/CombinedStyle.vue';
-import GlobalStyle from './components/GlobalStyle.vue';
-import LocalStyle from './components/LocalStyle.vue';
-import ModuleStyle from './components/ModuleStyle.vue';
+import EventHandler from './components/EventHandler.vue';
+
 </script>
 
 <template>
-  <GlobalStyle />
-  <LocalStyle />
-  <h3 class="comb-g-style">Combine Global and Local Style </h3>
-  <CombinedStyle />
-  <hr>
-  <ModuleStyle />
+  <EventHandler />
 </template>
---
+++ src/components/EventHandler.vue
@@ -0,0 +1,22 @@
+
+<script setup>
+import { ref } from "vue"
+
+// To add event we must need *reactive element*
+const count = ref(100)
+</script>
+
+<template>
+    <div>
+        <h2 >Counter: {{ count }}</h2>
+
+        <!-- v-on:eventName=eventMethod (old syntax)  -->
+        <button v-on:click="count++">Increment</button>
+
+        <!-- @eventName=eventMethod (new & shorthand syntax) -->
+        <button @click="count--">Decrement</button>
+    </div>
+</template>
+<style lang="scss" scoped>
+
+</style>
