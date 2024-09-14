161cdb684a5523aa006b696e2eb6fd623e8e830a
Hasem Mallick <hasemmallick19@students.cutn.ac.in>
2024-09-14 09:37:07 +0530

    code improvemtn

--- src/components/GetDataWithAxios.vue
+++ src/components/GetDataWithAxios.vue
@@ -7,13 +7,13 @@ const posts = ref(null)
 
 const getPosts = async () => {
     try {
-        const response = await axios.get('https://jsonplaceholder.typicode.cm/posts')
+        const response = await axios.get('https://jsonplaceholder.typicode.com/posts')
 
         // custom error handling
         // if (!response.status === 200) {
         //     throw new Error("HTTP Error", response.status);
         // }
-        
+
         // assign the response 
         posts.value = response.data
     } catch (error) {
@@ -25,8 +25,16 @@ const getPosts = async () => {
 <template>
     <div>
         <h2>Posts Request Response </h2>
-        <ol v-for="(post, index) in posts" :key="index" > 
+        <!-- <ol v-for="(post, index) in posts" :key="index" > 
             <li>{{ post }}</li>
+        </ol> -->
+
+        <!-- destructuring -->
+        <ol v-for="({id, userId, title, body}, index) in posts" :key="index" >
+            <li>{{ id }}</li>
+            <li>{{ userId }}</li>
+            <li>{{ title }}</li>
+            <li>{{ body }}</li>
         </ol>
         <button type="button" @click="getPosts">Get Posts</button>
     </div>
